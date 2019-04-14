f015d38f20d2a8ca275461a7893de86623def1b8
p.torkmand <p.torkmand@DESKTOP-H83GJ74>
2019-04-14 11:55:18 +0430

    compelet Api Actions

--- Form/Controllers/Api/ContactController.cs
+++ Form/Controllers/Api/ContactController.cs
@@ -1,29 +1,79 @@
-﻿using System;
+﻿using Form.Models;
+using Form.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Data.Entity;
 using System.Linq;
-using Form.Models;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Form.Controllers.Api
 {
     public class ContactController : Controller
     {
-        readonly string baseUrl = "Contact/GetContact";
-
-        [HttpPost]
-        public JsonResult GetContact([FromBody]Contact model,int counter=1)
+        [HttpGet]
+        public JsonResult GetContact(ContactViewModel model)
         {
             try
             {
                 TestProjectContext db = new TestProjectContext();
+
                 var contact = db.Contact.Where(a => !(a.Birthdate == null) &&
                                           !(a.Nid == null))
-                                          .OrderBy(a=>a.Nid)
-                                          .Skip(20 * (counter - 1)).Take(20).ToList();
+                                          .OrderBy(a => a.Nid
+                                          )
+                                          .Skip(20 * (model.Page - 1)).Take(20).ToList();
                 return Json(contact);
             }
             catch (Exception ex)
             {
+                return Json(ex);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult UpdateContact([FromBody]Contact model)
+        {
+            try
+            {
+                TestProjectContext db = new TestProjectContext();
+
+                var contact = db.Contact.Find(model.Id);
+                if (contact != null)
+                {
+                    contact.FirstName = model.FirstName;
+                    contact.LastName = model.LastName;
+                    contact.Nid = model.Nid;
+                    contact.Birthdate = model.Birthdate;
+
+                    db.SaveChanges();
+                }
+                return Json(contact);
+            }
+            catch (Exception ex)
+            {
+                return Json(ex);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult CreateContact([FromBody]Contact model)
+        {
+            try
+            {
+                TestProjectContext db = new TestProjectContext();
+                var contact = new Contact();
+                contact.FirstName = model.FirstName;
+                contact.LastName = model.LastName;
+                contact.Birthdate = model.Birthdate;
+                contact.Nid = model.Nid;
+
+                db.Contact.Add(contact);
+                db.SaveChanges();
+
+                return Json(true);
+            }
+            catch (Exception ex)
+            {
+
                 return Json(ex);
             }
         }
---
+++ Form/Models/ViewModels/ContactViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Form.Models.ViewModels
+{
+    public class ContactViewModel
+    {
+        public int Page { get; set; } = 1;
+    }
+}
